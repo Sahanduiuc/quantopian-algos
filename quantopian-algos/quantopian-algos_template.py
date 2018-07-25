bcd6d1d3ff8ae8824067d32a622a525ead42f3c0
romaincaraes <hello@romaincaraes.fr>
2018-07-25 22:47:46 +0200

    Add first template
    Create quantopian-algos/ directory and add quantopian-algos_template.py

---
+++ quantopian-algos/quantopian-algos_template.py
@@ -0,0 +1,14 @@
+import quantopian.algorithm
+import quantopian.pipeline
+
+def initialize(context):
+    pass
+
+def make_pipeline():
+    pass
+
+def before_trading_start(context, data):
+    pass
+
+def handle_data(context,data):
+    pass
